b21897eaf6817f17ce4e41c385ff85692f34961a
celia <uo277578@uniovi.es>
2022-11-18 15:14:04 +0100

    Historial con bd pero no hay datos

--- app/src/main/java/com/example/wip/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/wip/ui/gallery/GalleryFragment.java
@@ -8,24 +8,40 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.wip.R;
+import com.example.wip.data.FiestasDataSource;
 import com.example.wip.databinding.FragmentGalleryBinding;
+import com.example.wip.layouts.ListaFragments;
+import com.example.wip.modelo.Fiesta;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
+    private View root;
+    private List<Fiesta> fiestas = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
+        /*GalleryViewModel galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);*/
+
+        root = inflater.inflate(R.layout.activity_fragment, container, false);
+        getFavPartys();
+
+        loadFragment(ListaFragments.newInstance((ArrayList<Fiesta>) fiestas));
         return root;
     }
 
@@ -33,5 +49,21 @@ public class GalleryFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void getFavPartys(){
+        FiestasDataSource fiestasDataSource = new FiestasDataSource(getContext());
+        fiestasDataSource.open();
+        fiestas = fiestasDataSource.getAllValorations();
+        fiestasDataSource.close();
+
+
+    }
+
+    private void loadFragment(Fragment fragment){
+
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
     }
 }
