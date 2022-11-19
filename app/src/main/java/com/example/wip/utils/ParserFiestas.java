9b8935da52dfbfc24c88bc8c9d9fa9cf620576fc
Sergio Castillo García <UO276436@uniovi.es>
2022-11-19 14:59:29 +0100

    Se pueden ver fiestas de lugares expecificos (ejemplo:Luarca)

--- app/src/main/java/com/example/wip/utils/ParserFiestas.java
+++ app/src/main/java/com/example/wip/utils/ParserFiestas.java
@@ -8,9 +8,9 @@ public class ParserFiestas {
 
     public static ArrayList<Fiesta> ParseFiestas(String resultado) {
         ArrayList<Fiesta> fiestas = new ArrayList<Fiesta>();
-        String[] splitted = resultado.split("<li>|<li");
-        //Las fiestas siemprea se encuentras entre estas posiciones: -35 -25
-        for (int i = splitted.length - 35; i < splitted.length - 25; i++) {
+        String[] splitted = resultado.split("<ul id=\"fiestas\">|<div id=\"pagination\">");
+        splitted = splitted[1].split("<li>|<li");
+        for (int i = 1; i < splitted.length ; i++) {
             //Parseamos una fiesta y la añadimos al arary
             Fiesta fiesta = parseFiesta(splitted[i]);
             fiestas.add(fiesta);
@@ -22,7 +22,7 @@ public class ParserFiestas {
     private static Fiesta parseFiesta(String texto) {
         Fiesta fiesta = new Fiesta();
         //Parseamos, gracias a las clases, ademas quitamos los <a>, <span> y \n
-        fiesta.setPlace(texto.split("class=\"town\">")[1].split("</a>|</span>")[0]);
+        fiesta.setPlace(texto.split("class=\"town\">|class=\"local\">")[1].split("</a>|</span>")[0].replace("&gt;",""));
         fiesta.setDate(texto.split("class=\"dates\">")[1].split("</span>")[0].split("\n")[0]);
         fiesta.setName(texto.split("class=\"name\">")[1].split("</a>|</span>")[0]);
         try{
